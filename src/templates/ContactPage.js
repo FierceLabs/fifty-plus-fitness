e5abd203739e037b53d3c10c255d46f0a5f4255b
Jared Nelson <roial33@hotmail.com>
2021-04-19 20:42:56 -0600

    Services

--- src/templates/ContactPage.js
+++ src/templates/ContactPage.js
@@ -61,8 +61,16 @@ export const ContactPageTemplate = ({
         </div>
       </div>
     </section>
-
-    <GoogleMap locations={locations} />
+    <div className="mapping">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.4277176992878!2d-116.56420098453727!3d43.57680656538521!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54ae4c6c75bba291%3A0xc6a23bba6a31aee!2s314%2012th%20Ave%20S%2C%20Nampa%2C%20ID%2083651!5e0!3m2!1sen!2sus!4v1618876874729!5m2!1sen!2sus"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+    </div>
   </main>
 )
 
@@ -90,11 +98,6 @@ export const pageQuery = graphql`
         address
         phone
         email
-        locations {
-          mapLink
-          lat
-          lng
-        }
       }
     }
   }
